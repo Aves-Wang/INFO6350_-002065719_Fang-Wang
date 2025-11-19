5368274c4ac38f982d80aa5a90a2018a30e1a43f
Aves-Wang <wang.f2@northeastern.edu>
2025-11-18 19:12:21 -0800

    Create calculator_core.dart

---
+++ exercises/exercise6a/calculator_core.dart
@@ -0,0 +1,8 @@
+/ lib/calculator_core.dart
+double add(double a, double b) => a + b;
+double subtract(double a, double b) => a - b;
+double multiply(double a, double b) => a * b;
+double divide(double a, double b) {
+  if (b == 0) throw ArgumentError('Division by zero');
+  return a / b;
+}
